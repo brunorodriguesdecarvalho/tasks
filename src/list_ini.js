9b2f20edaf49296a80814882e11b5db9cda340eb
brunorodriguesdecarvalho <rodrigues_b@hotmail.com>
2020-02-23 16:00:26 -0300

    Implantação do RAG Status.

--- src/list_ini.js
+++ src/list_ini.js
@@ -8,25 +8,48 @@ function getIniciativas() {
 getIniciativas()
 
 function listarIniciativas(iniciativas){
-    $("#Ini").append(
-        `<ul class="item">
-            <h2><strong>${iniciativas.iniNome}</strong></h2>
-            <li><strong>Objetivo associado: </strong>${iniciativas.iniObj}</li> 
-            <li><strong>Status atual: </strong>${iniciativas.iniStat}</li>
+    $("#Ini").append(`
+        <div class="linha">
+            <div id="bola-${iniciativas._id}" class="celula" style="
+                border: none; 
+                height: 28px; 
+                width: 28px; 
+                border-radius:30px;
+                vertical-align: middle;
+            ">
+            </div>
+            <div class="celula" style="vertical-align: middle;">
+                <p style="
+                    font-size: 28px; 
+                    font-weight: bold;
+                    margin-block-start: 0em;
+                    margin-block-end: 0em;
+                ">${iniciativas.iniNome}</p>
+            </div>
+        </div>
+        <ul> 
+            <li><strong>Objetivo associado: </strong>${iniciativas.iniObj}</li>
+            <li id="Stat-${iniciativas.iniStat}" data-value="${iniciativas.iniStat}">
+                <strong>Status atual: </strong>
+                ${iniciativas.iniStat}
+            </li>
             <li><strong>Prazo: </strong>${iniciativas.iniDataFim}</li>
             <li><strong>Descrição: </strong>${iniciativas.iniDesc}</li>
             <li><strong>Motivo(s): </strong>${iniciativas.iniMot}</li>
             <li><strong>Risco(s): </strong>${iniciativas.iniRisk}</li>
             <li>Início: ${iniciativas.iniDataCria}</li>
-            <li>ID: ${iniciativas._id}</li>
-            <li><a href="#" onclick="hello()">Excluir</a></li>
         </ul>
+        <script>
+            function hello() {
+                var valorStat = document.getElementById("Stat-${iniciativas.iniStat}").getAttribute("data-value");
+                if (valorStat == "-1: Urgente") document.getElementById('bola-${iniciativas._id}').style.backgroundColor='#DC0000';
+                else if (valorStat == "0 - Atrasado") document.getElementById('bola-${iniciativas._id}').style.backgroundColor='#DCA000' 
+                else if (valorStat == "1 - Não Iniciado") document.getElementById('bola-${iniciativas._id}').style.backgroundColor='#000000'
+                else if (valorStat == "2 - Em Andamento") document.getElementById('bola-${iniciativas._id}').style.backgroundColor='#009600'
+                else if(valorStat == "3 - Concluído") document.getElementById('bola-${iniciativas._id}').style.backgroundColor='#00008C'
+            };
+            hello()
+        </script>
         <br>
     `)
 }
-
-$(document).ready(function() {
-    window.hello = function() {
-      alert("oi");
-    }
-  });
--- src/list_obj.js
+++ src/list_obj.js
@@ -8,18 +8,48 @@ function getObjetivos() {
 getObjetivos()
 
 function listarObjetivos(objetivos){
-    $("#Obj").append(
-        `<ul class="item">
-            <h2><strong>${objetivos.objNome}</strong></h2>
+    $("#Obj").append(`
+        <div class="linha">
+            <div id="bola-${objetivos._id}" class="celula" style="
+                border: none; 
+                height: 28px; 
+                width: 28px; 
+                border-radius:30px;
+                vertical-align: middle;
+            ">
+            </div>
+            <div class="celula" style="vertical-align: middle;">
+                <p style="
+                    font-size: 28px; 
+                    font-weight: bold;
+                    margin-block-start: 0em;
+                    margin-block-end: 0em;
+                ">${objetivos.objNome}</p>
+            </div>
+        </div>
+        <ul class="item">
             <li><strong>Tema associado: </strong>${objetivos.objTema}</li>
-            <li><strong>Status atual: </strong>${objetivos.objStat}</li>
+            <li id="Stat-${objetivos.objStat}" data-value="${objetivos.objStat}">
+                <strong>Status atual: </strong>
+                ${objetivos.objStat}
+            </li>
             <li><strong>Prazo: </strong>${objetivos.objDataFim}</li>
             <li><strong>Descrição: </strong>${objetivos.objDesc}</li>
             <li><strong>Motivo(s): </strong>${objetivos.objMot}</li>
             <li><strong>Risco(s): </strong>${objetivos.objRisk}</li>
             <li>Início: ${objetivos.objDataCria}</li>
-            <li>ID: ${objetivos._id}</li>
         </ul>
+        <script>
+            function hello() {
+                var valorStat = document.getElementById("Stat-${objetivos.objStat}").getAttribute("data-value");
+                if (valorStat == "-1: Urgente") document.getElementById('bola-${objetivos._id}').style.backgroundColor='#DC0000';
+                else if (valorStat == "0 - Atrasado") document.getElementById('bola-${objetivos._id}').style.backgroundColor='#DCA000' 
+                else if (valorStat == "1 - Não Iniciado") document.getElementById('bola-${objetivos._id}').style.backgroundColor='#000000'
+                else if (valorStat == "2 - Em Andamento") document.getElementById('bola-${objetivos._id}').style.backgroundColor='#009600'
+                else if(valorStat == "3 - Concluído") document.getElementById('bola-${objetivos._id}').style.backgroundColor='#00008C'
+            };
+            hello()
+        </script>
         <br>
     `)
 }
